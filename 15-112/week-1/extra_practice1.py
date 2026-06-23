8ebef594684a8a5194b632c937d73247da25dbf5
rootkit <339551677@qq.com>
2026-06-23 22:33:01 +0800

    Implement various geometric and fabric functions

--- 15-112/week-1/extra_practice1.py
+++ 15-112/week-1/extra_practice1.py
@@ -5,10 +5,8 @@
 # Your andrew id:
 #################################################
 
-import decimal
-import math
-
 import cs112_f22_week1_linter
+import math
 
 #################################################
 # Helper functions
@@ -18,6 +16,7 @@ def almostEqual(d1, d2, epsilon=10**-7):
     # note: use math.isclose() outside 15-112 with Python version 3.5 or later
     return (abs(d2 - d1) < epsilon)
 
+import decimal
 def roundHalfUp(d):
     # Round to nearest with ties going away from zero.
     rounding = decimal.ROUND_HALF_UP
@@ -29,38 +28,90 @@ def roundHalfUp(d):
 # Functions for you to write
 #################################################
 
-def fabricYards(inches):
-    return 42
+def distance(x1, y1, x2, y2):
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
+def coordinateToLine(x1, y1, x2, y2, x3, y3):
+    triangle_a = distance(x1, y1, x2, y2)
+    triangle_b = distance(x1, y1, x3, y3)
+    triangle_c = distance(x2, y2, x3, y3)
+    return triangle_a, triangle_b, triangle_c
+
+def triangleLine(s1, s2, s3):
+    min_triangle = min(s1, s2, s3)
+    max_triangle = max(s1, s2, s3)
+    mid_triangle = s1 + s2 + s3 - min_triangle - max_triangle
+    return min_triangle, mid_triangle, max_triangle
+
+def fabricYards(inches):
+    if inches % 36 == 0:
+        return int(inches / 36)
+    return int(inches / 36) + 1
+ 
 def fabricExcess(inches):
-    return 42
+    return fabricYards(inches) * 36 - inches
 
 def isEvenPositiveInt(x):
-    return 42
+    # if isinstance(x, int) and (x > 0) and (x % 2 == 0):
+    #     return True
+    # return False
+    return isinstance(x, int) and (x > 0) and (x % 2 == 0)
 
 def nthFibonacciNumber(n):
-    return 42
+    if n == 0 or n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        return roundHalfUp(((1 + math.sqrt(5)) ** n - (1 - math.sqrt(5)) ** n) / (2 ** n * math.sqrt(5)))
 
 def isLegalTriangle(s1,s2,s3):
-    return 42
+    if s1 > 0 and s2 > 0 and s3 > 0:
+        min_triangle, mid_triangle, max_triangle = triangleLine(s1, s2, s3)
+        if mid_triangle + min_triangle > max_triangle:
+            return True
+    return False
 
 def isRightTriangle(x1, y1, x2, y2, x3, y3):
-    return 42
+    triangle_a, triangle_b, triangle_c = coordinateToLine(x1, y1, x2, y2, x3, y3)
+    min_triangle, mid_triangle, max_triangle = triangleLine(
+        triangle_a, triangle_b, triangle_c)
+    if almostEqual(math.sqrt(min_triangle ** 2 + mid_triangle ** 2), max_triangle):
+        return True
+    return False
 
 def triangleArea(s1, s2, s3):
-    return 42
+    min_triangle, mid_triangle, max_triangle = triangleLine(s1, s2, s3)
+    if isLegalTriangle(max_triangle, min_triangle, mid_triangle):
+        s = (s1 + s2 + s3) / 2
+        return math.sqrt(s * (s - s1) * (s - s2) * (s - s3))
+    return 0
 
 def triangleAreaByCoordinates(x1, y1, x2, y2, x3, y3):
-    return 42
+    s1, s2, s3 = coordinateToLine(x1, y1, x2, y2, x3, y3)
+    return triangleArea(s1, s2, s3)
 
 def lineIntersection(m1, b1, m2, b2):
-    return 42
+    if m1 == m2:
+        return None
+    return (b2 - b1) / (m1 - m2)
+
+def lineIntersect(m1, b1, m2, b2):
+    return (b2 - b1) / (m1 - m2), m1 * ((b2 - b1) / (m1 - m2)) + b1
 
 def threeLinesArea(m1, b1, m2, b2, m3, b3):
-    return 42
+    if m1 != m2 and m1 != m3 and m2 != m3:
+        x1, y1 = lineIntersect(m1, b1, m2, b2)
+        x2, y2 = lineIntersect(m1, b1, m3, b3)
+        x3, y3 = lineIntersect(m2, b2, m3, b3)
+        s1, s2, s3 = coordinateToLine(x1, y1, x2, y2, x3, y3)
+        return triangleArea(s1, s2, s3)
+    return 0
 
 def rectanglesOverlap(x1, y1, w1, h1, x2, y2, w2, h2):
-    return 42
+    if (y1 > y2 + h2 or y2 > y1 + h1 or x1 > x2 + w2 or x2 > x1 + w1):
+        return False
+    return True
 
 #################################################
 # Test Functions
@@ -78,7 +129,7 @@ def testFabricYards():
     assert(fabricYards(108) == 3)
     assert(fabricYards(109) == 4)
     print('Passed.')
-
+ 
 def testFabricExcess():
     print('Testing fabricExcess()... ', end='')
     assert(fabricExcess(0) == 0)
